2cf545b5a0d96cbd50e71b683643715ba01c1b9c
NoahMSchool <19noahm@hollandparkschool.co.uk>
2025-05-05 11:11:40 +0100

    Added Checkpoint animation and started stages

--- Scenes/WorldScenes/game_manager.gd
+++ Scenes/WorldScenes/game_manager.gd
@@ -2,6 +2,14 @@ extends Node
 
 signal reset_level
 
+var stage_num = 0
+
+var stage = [$"../Stages/Stage1", ]
+
 func player_die():
 	print("die")
 	emit_signal("reset_level")
+
+func stage_reached():
+	for node in stage[stage_num]:
+		stage_num +=1
--- Scenes/check_point_spawn.gd
+++ Scenes/check_point_spawn.gd
@@ -1,9 +1,10 @@
 extends Node3D
-
-
-
+@export var barrier : StaticBody3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		body.spawnpoint = self
-		$Area3D.disabled = true
+	if barrier:
+		barrier.get_node("CollisionShape3D").set_deferred("disabled", false)
+		barrier.get_node("MeshInstance3D").visible = true
+	$CheckPointRing/AnimationPlayer.play("ringFly")
