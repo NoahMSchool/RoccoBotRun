c03072f2b7ee7daf1cd78f7984e1ea0c897ab321
NoahMSchool <19noahm@hollandparkschool.co.uk>
2025-05-05 21:36:02 +0100

    Initial Stages with stagenum

--- Scenes/WorldScenes/game_manager.gd
+++ Scenes/WorldScenes/game_manager.gd
@@ -2,14 +2,47 @@ extends Node
 
 signal reset_level
 
-var stage_num = 0
+@export var stage_node : Node
+var stage_num : int = 0
+@onready var stages = stage_node.get_children()
+@export var player_node : CharacterBody3D
 
-var stage = [$"../Stages/Stage1", ]
 
 func player_die():
 	print("die")
 	emit_signal("reset_level")
 
 func stage_reached():
-	for node in stage[stage_num]:
+	for node in stages[stage_num]:
 		stage_num +=1
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("plus"):
+		stage_num = (stage_num+1) % (stages.size())
+		change_stage()
+		
+	if event.is_action_pressed("minus"):
+		stage_num = absi((stage_num-1) % (stages.size()))
+		change_stage()
+	if event.is_action_pressed("FreeMouse"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("Die"):
+		player_die()
+		
+func change_stage():
+	print(stage_num)
+	print(stages[stage_num].name)
+	for i in range(stages.size()):
+		var stage_object = stages[i].get_node("StageObjects")
+		if stage_object:
+			stage_object.visible = (i == stage_num)
+		
+	var spawn_object = stages[stage_num].get_node("CheckPointSpawn")
+		#spawn_object.visible = (i == stage_num)
+	if spawn_object && player_node:
+		player_node.spawnpoint = spawn_object
+		
+
+
+func _on_check_point_spawn_checkpoint_reached(checkpoint: Node3D) -> void:
+	print(checkpoint)
--- Scenes/check_point_spawn.gd
+++ Scenes/check_point_spawn.gd
@@ -1,10 +1,16 @@
 extends Node3D
+
+signal checkpoint_reached(checkpoint: Node3D)
+
 @export var barrier : StaticBody3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
+		checkpoint_reached.emit(self)
 		body.spawnpoint = self
 	if barrier:
 		barrier.get_node("CollisionShape3D").set_deferred("disabled", false)
 		barrier.get_node("MeshInstance3D").visible = true
 	$CheckPointRing/AnimationPlayer.play("ringFly")
+	
+	
