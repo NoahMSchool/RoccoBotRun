87c5a2517b39ebcaae16afdbd45a5c7fd3a6fe4c
NoahMSchool <19noahm@hollandparkschool.co.uk>
2025-06-19 22:47:17 +0100

    Added Laser Sheild shader and got shield working

--- deal_damage_component.gd
+++ deal_damage_component.gd
@@ -5,16 +5,9 @@ class_name DealDamageComponent
 @export var target_group = "enemies"
 
 func deal_damage(damage, body):
-	# Step 1: find the node that actually has the component
 	var target : Node = body
-	# If you collided with an Area3D under the enemy, move to its parent.
-	# But we don’t hardcode Area3D; we just look for the component.
 	while target and not target.has_node("DamagableComponent"):
 		target = target.get_parent()
-	if not target:
-		return  # no damageable component found anywhere up the chain
-
-
-	
-	if target.is_in_group(target_group) and target.has_node("DamagableComponent"):
-			target.damagable_component.get_damaged(damage)
+	if target:
+		if target.is_in_group(target_group) and target.has_node("DamagableComponent"):
+				target.damagable_component.get_damaged(damage)
