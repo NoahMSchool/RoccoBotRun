004dc1b6a60fe3baddcddc6dd6be692a6a621864
NoahMSchool <19noahm@hollandparkschool.co.uk>
2026-08-20 22:22:34 +0100

    made game root and cities more generic and moved environment objects

--- GameMain/game_root.gd
+++ GameMain/game_root.gd
@@ -1,7 +1,16 @@
 extends Node
 
 @onready var player_node : Node3D
-@onready var current_planet : PlanetWorld = $SpaceCity
+
+var current_planet_num = 0
+
+@onready var planet_container: Node3D = $PlanetContainer
+
+var planets = [[preload("res://Environment/PlanetScenes/space_city.tscn"), null]
+			]
+
+func _ready() -> void:
+	load_current_planet()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("FreeMouse"):
@@ -27,3 +36,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if event.is_action_pressed("quit") and event.is_pressed() and not event.is_echo():
 		get_tree().quit()
+
+func instantiate_planet(planet_num):
+	var new_planet = planets[planet_num][0].instantiate()
+	planets[planet_num][1] = new_planet
+
+func load_current_planet():
+	if planets[current_planet_num][1] == null:
+		instantiate_planet(current_planet_num)
+	var planet_node = planets[current_planet_num][1]
+	if planet_container.get_child_count()>0:
+		planet_container.remove_child(planet_container.get_child(0))
+	planet_container.add_child(planet_node)
+	
